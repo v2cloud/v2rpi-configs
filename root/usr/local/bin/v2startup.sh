b39ad799426abc19e85a0d55f8fe8dd8b9c2cd5a
nvdhau-v2cloud <hau@v2cloud.com>
2021-06-16 23:17:45 -0700

    save v2startup_log file

--- root/usr/local/bin/v2startup.sh
+++ root/usr/local/bin/v2startup.sh
@@ -2,4 +2,4 @@
 # This file should not be change
 
 # execute v2cloud.sh script correctly (because the file may be overwritten during runtime)
-cat /usr/local/bin/v2cloud.sh | bash
+cat /usr/local/bin/v2cloud.sh | bash &> /tmp/v2startup_log 
